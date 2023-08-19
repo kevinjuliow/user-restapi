5badf56b19e868faff353c77600f5af9be424b00
kevinjuliow <kevjuliow@gmail.com>
2023-08-19 18:46:25 +0700

    first commit

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.models.User;
 import com.example.demo.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
