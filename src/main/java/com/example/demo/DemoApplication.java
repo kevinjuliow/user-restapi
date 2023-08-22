0b44bad812200a8d25a3b3be96124041fd008c9c
Kevin Ju <kevjuliow@gmail.com>
2023-08-22 21:04:27 +0700

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class DemoApplication {
 
 	public static void main(String[] args) {
